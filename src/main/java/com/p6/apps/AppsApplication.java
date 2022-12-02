c8680fee85448d99d2df7ea1f73a23f14e2807e8
Idohb <gmooocgel@gmail.com>
2022-12-02 15:51:34 +0100

    First Commit

---
+++ src/main/java/com/p6/apps/AppsApplication.java
@@ -0,0 +1,13 @@
+package com.p6.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppsApplication.class, args);
+    }
+
+}
