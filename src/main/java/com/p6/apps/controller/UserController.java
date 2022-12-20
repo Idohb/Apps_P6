d6d5711aa2a31dee5e33f9ceeb764b694d92fcb5
Idohb <gmooocgel@gmail.com>
2022-12-20 17:44:56 +0100

    Add register method and login method

--- src/main/java/com/p6/apps/controller/UserController.java
+++ src/main/java/com/p6/apps/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.p6.apps.controller;
+import com.p6.apps.controller.dto.user.RegisterRequest;
 import com.p6.apps.service.UserService;
 import com.p6.apps.service.data.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -29,6 +29,20 @@ public class UserController {
         } catch (NoSuchElementException e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("login/{id}")
+    public ResponseEntity<User> getLogin(@PathVariable("id") final Long id) {
+        try {
+            return ResponseEntity.ok(userService.getLogin(id));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("signon")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest user) {
+        return ResponseEntity.ok(userService.addUser(user));
     }
 
 }
--- src/main/java/com/p6/apps/service/UserService.java
+++ src/main/java/com/p6/apps/service/UserService.java
@@ -1,11 +1,15 @@
 package com.p6.apps.service;
 
+import com.p6.apps.controller.dto.user.RegisterRequest;
 import com.p6.apps.mapper.UserConverter;
+import com.p6.apps.model.entity.UserEntity;
 import com.p6.apps.model.repository.UserRepository;
 import com.p6.apps.service.data.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -20,5 +24,23 @@ public class UserService {
 
     public List<User> getLogins() {
         return userConverter.mapperUser(userRepository.findAll());
+    }
+
+    public User getLogin(final Long id) {
+        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Id " + id + " not found"));
+        return userConverter.mapperUser(userEntity);
+    }
+
+    public User addUser(RegisterRequest userRequest) {
+        UserEntity userEntity = new UserEntity(0L,
+                userRequest.getFirst_name(),
+                userRequest.getLast_name(),
+                userRequest.getEmail(),
+                userRequest.getPassword(),
+                0,
+                new ArrayList<>()
+        );
+        userRepository.save(userEntity);
+        return userConverter.mapperUser(userEntity);
     }
 }
