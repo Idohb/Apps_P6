1f054870b3672345cf29af08677a64f64b3c3cec
Idohb <gmooocgel@gmail.com>
2022-12-21 18:03:51 +0100

    Add basic operation to add a friend

--- src/main/java/com/p6/apps/controller/UserController.java
+++ src/main/java/com/p6/apps/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.p6.apps.controller;
+import com.p6.apps.service.FriendService;
 import com.p6.apps.service.UserService;
 import com.p6.apps.service.data.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +19,13 @@ import java.util.NoSuchElementException;
 public class UserController {
 
     private final UserService userService;
+    private final FriendService friendService;
+
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, FriendService friendService) {
         this.userService = userService;
+        this.friendService = friendService;
     }
 
     @GetMapping("logins")
@@ -31,4 +37,12 @@ public class UserController {
         }
     }
 
+    @PostMapping("addFriend")
+    public ResponseEntity<Long> addFriend(@RequestParam("creditor")final Long creditorId, @RequestParam("debtor")final Long debtorId) {
+        try {
+            return ResponseEntity.ok(friendService.addFriend(creditorId,debtorId));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
---
+++ src/main/java/com/p6/apps/service/FriendService.java
@@ -0,0 +1,25 @@
+package com.p6.apps.service;
+import com.p6.apps.model.entity.UserEntity;
+import com.p6.apps.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class FriendService {
+    @Autowired
+    UserRepository userRepository;
+
+    public Long addFriend(Long lCreditor, Long lDebtor) {
+        List<UserEntity> userEntityList;
+        UserEntity usrCreditor = userRepository.findById(lCreditor).orElseThrow( () -> new NoSuchElementException("") );
+        UserEntity usrDebdtor = userRepository.findById(lDebtor).orElseThrow( () -> new NoSuchElementException("") );
+        userEntityList = usrCreditor.getFriend();
+        userEntityList.add(usrDebdtor);
+        usrCreditor.setFriend(userEntityList);
+        userRepository.save(usrCreditor);
+        return 0L;
+    }
+}
