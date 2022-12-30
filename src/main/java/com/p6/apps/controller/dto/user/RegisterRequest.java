8ccf9e6d17450753025f71b4516f038f6f104102
Idohb <gmooocgel@gmail.com>
2022-12-30 11:11:55 +0100

    Add feature friend to develop

---
+++ src/main/java/com/p6/apps/controller/dto/user/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.p6.apps.controller.dto.user;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RegisterRequest {
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+}
--- src/main/java/com/p6/apps/service/UserService.java
+++ src/main/java/com/p6/apps/service/UserService.java
@@ -1,14 +1,19 @@
 package com.p6.apps.service;
 
+import com.p6.apps.controller.dto.user.RegisterRequest;
 import com.p6.apps.mapper.UserConverter;
+import com.p6.apps.model.entity.UserEntity;
 import com.p6.apps.model.repository.UserRepository;
 import com.p6.apps.service.data.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService extends Exception {
 
     private final UserConverter userConverter;
     private final UserRepository userRepository;
@@ -21,4 +26,32 @@ public class UserService {
     public List<User> getLogins() {
         return userConverter.mapperUser(userRepository.findAll());
     }
+
+    public User getLogin(final Long id) {
+        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Id " + id + " not found"));
+        return userConverter.mapperUser(userEntity);
+    }
+
+    public User addUser(RegisterRequest userRequest) {
+        //create an custom exception
+        if(!this.verifyEmailRedundant(userRequest)) { return new User();}
+        UserEntity userEntity = new UserEntity(0L,
+                userRequest.getFirst_name(),
+                userRequest.getLast_name(),
+                userRequest.getEmail(),
+                userRequest.getPassword(),
+                0,
+                new ArrayList<>(), // debtor
+                new ArrayList<>(), // creditor
+                new ArrayList<>() // friend
+        );
+        UserEntity entity = userRepository.save(userEntity);
+        return userConverter.mapperUser(entity);
+    }
+
+    private boolean verifyEmailRedundant (RegisterRequest userRequest){
+            Optional<UserEntity> usrNonRedundant = userRepository.findByEmail(userRequest.getEmail());
+            return usrNonRedundant.isEmpty();
+    }
 }
+
